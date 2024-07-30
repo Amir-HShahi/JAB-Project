53cdf06e3825c6662a77763992d1541b13c07bd1
Amir-HShahi <amir13831107h@gmail.com>
2024-07-31 01:17:58 +0330

    SignUpScreen: completed

--- lib/view/components/sign_up_button_widget.dart
+++ lib/view/components/sign_up_button_widget.dart
@@ -3,7 +3,10 @@ import 'package:flutter/material.dart';
 class SignUpButtonWidget extends StatelessWidget {
   final String buttonImagePath;
   final Function() pressButtonFunction;
-  const SignUpButtonWidget({super.key, required this.pressButtonFunction, required this.buttonImagePath});
+  const SignUpButtonWidget(
+      {super.key,
+      required this.pressButtonFunction,
+      required this.buttonImagePath});
 
   @override
   Widget build(BuildContext context) {
@@ -13,10 +16,17 @@ class SignUpButtonWidget extends StatelessWidget {
         height: 60,
         width: 60,
         decoration: BoxDecoration(
-          color: Colors.white,
-          borderRadius: const BorderRadius.all(Radius.circular(10)),
-          border: Border.all(color: Colors.black),
-        ),
+            color: Colors.white,
+            borderRadius: const BorderRadius.all(Radius.circular(10)),
+            border: Border.all(color: Colors.black),
+            boxShadow: [
+              BoxShadow(
+                color: Colors.grey.withOpacity(0.1),
+                spreadRadius: 5,
+                blurRadius: 7,
+                offset: const Offset(0, 3), // changes position of shadow
+              ),
+            ]),
         padding: const EdgeInsets.all(15),
         child: Image(image: AssetImage(buttonImagePath)),
       ),
--- lib/view/sign_up_screen.dart
+++ lib/view/sign_up_screen.dart
@@ -1,5 +1,6 @@
 import 'package:flutter/material.dart';
 import 'package:google_fonts/google_fonts.dart';
+import 'package:jab_project/view/components/sign_styled_text_field.dart';
 import 'package:jab_project/view/components/sign_up_button_widget.dart';
 
 class SignUpScreen extends StatelessWidget {
@@ -8,6 +9,10 @@ class SignUpScreen extends StatelessWidget {
   void signUpWithGoogleHandler() {}
 
   void signUpWithAppleHandler() {}
+
+  void signUpButtonHandler() {}
+
+  void loginToAccountHandler() {}
 
   @override
   Widget build(BuildContext context) {
@@ -24,6 +29,42 @@ class SignUpScreen extends StatelessWidget {
                   style: GoogleFonts.workSans(
                       fontWeight: FontWeight.bold, fontSize: 30)),
               const SizedBox(height: 40),
+              const SignStyledTextField(
+                  hintText: " Email Address", isSuffixIconActive: false),
+              const SizedBox(height: 20),
+              const SignStyledTextField(
+                  hintText: " Username", isSuffixIconActive: false),
+              const SizedBox(height: 20),
+              const SignStyledTextField(
+                  hintText: " Phone", isSuffixIconActive: false),
+              const SizedBox(height: 20),
+              const SignStyledTextField(
+                  hintText: " Password", isSuffixIconActive: true),
+              const SizedBox(height: 20),
+              Row(
+                children: [
+                  Expanded(
+                    child: Container(
+                      height: 1,
+                      color: Colors.grey,
+                    ),
+                  ),
+                  const SizedBox(width: 10),
+                  Text("or",
+                      style: GoogleFonts.workSans(
+                          fontWeight: FontWeight.normal,
+                          fontSize: 20,
+                          color: Colors.grey)),
+                  const SizedBox(width: 10),
+                  Expanded(
+                    child: Container(
+                      height: 1,
+                      color: Colors.grey,
+                    ),
+                  ),
+                ],
+              ),
+              const SizedBox(height: 20),
               Center(
                 child: Row(
                   mainAxisAlignment: MainAxisAlignment.center,
@@ -35,6 +76,52 @@ class SignUpScreen extends StatelessWidget {
                     SignUpButtonWidget(
                         pressButtonFunction: signUpWithAppleHandler,
                         buttonImagePath: "assets/images/apple-logo.png")
+                  ],
+                ),
+              ),
+              const SizedBox(height: 50),
+              GestureDetector(
+                onTap: signUpButtonHandler,
+                child: Container(
+                  height: 70,
+                  width: double.infinity,
+                  decoration: const BoxDecoration(
+                      color: Color(0xff0098FF),
+                      borderRadius: BorderRadius.all(Radius.circular(10))),
+                  child: Row(
+                    mainAxisAlignment: MainAxisAlignment.center,
+                    children: [
+                      Text("Sign Up",
+                          style: GoogleFonts.workSans(
+                              fontWeight: FontWeight.bold,
+                              fontSize: 25,
+                              color: Colors.white)),
+                      const SizedBox(width: 16),
+                      const Icon(Icons.arrow_forward_rounded,
+                          size: 20, color: Colors.white)
+                    ],
+                  ),
+                ),
+              ),
+              const SizedBox(height: 20),
+              Center(
+                child: Row(
+                  mainAxisAlignment: MainAxisAlignment.center,
+                  children: [
+                    Text("Already have an account?",
+                        style: GoogleFonts.workSans(
+                          fontWeight: FontWeight.normal,
+                          fontSize: 16,
+                        )),
+                    GestureDetector(
+                      onTap: loginToAccountHandler,
+                      child: Text(" Login to account",
+                          style: GoogleFonts.workSans(
+                            fontWeight: FontWeight.normal,
+                            fontSize: 16,
+                            color: const Color(0xff0098FF),
+                          )),
+                    )
                   ],
                 ),
               )
