8ac1307326e93a4bee7bb84ec6f7c14e44c4a087
Amir-HShahi <amir13831107h@gmail.com>
2024-08-07 21:47:30 +0330

    ProfileScreen: added to routes

--- lib/main.dart
+++ lib/main.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:jab_project/view/profile_screen.dart';
 import 'package:jab_project/view/sign_in_screen.dart';
 import 'package:jab_project/view/sign_up_screen.dart';
 
@@ -16,6 +17,7 @@ class MyApp extends StatelessWidget {
       routes: {
         '/SignInScreen' : (context) => const SignInScreen(),
         '/SignUpScreen' : (context) => const SignUpScreen(),
+        '/ProfileScreen' : (context) => const ProfileScreen(),
       },
     );
   }
