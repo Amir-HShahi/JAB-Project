0d7d6bee704c5e4b8d502ec8926c5b67ecbb9939
Amir-HShahi <amir13831107h@gmail.com>
2024-08-07 21:34:55 +0330

    ProfileScreen: base implemented

--- lib/view/profile_screen.dart
+++ lib/view/profile_screen.dart
@@ -1,10 +1,22 @@
-import 'package:flutter/cupertino.dart';
+import 'package:flutter/material.dart';
 
 class ProfileScreen extends StatelessWidget {
   const ProfileScreen({super.key});
 
   @override
   Widget build(BuildContext context) {
-    return const Placeholder();
+    return SafeArea(
+        child: Scaffold(
+      backgroundColor: const Color(0xfff2faff),
+      body: SingleChildScrollView(
+        child: Container(
+          width: double.infinity,
+          padding: const EdgeInsets.only(top: 60, left: 20, right: 20),
+          child: const Column(
+            children: [],
+          ),
+        ),
+      ),
+    ));
   }
 }
