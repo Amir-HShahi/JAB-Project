ced8411fb23c4eb85bd639e6b92bd64ea452f92c
Amir-HShahi <amir13831107h@gmail.com>
2024-08-11 23:52:22 +0330

    SignInScreen: notification bar color correction

--- lib/view/sign_in_screen.dart
+++ lib/view/sign_in_screen.dart
@@ -22,101 +22,102 @@ class SignInScreen extends StatelessWidget {
       Navigator.pushNamed(context, '/ProfileScreen');
     }
 
-    return SafeArea(
-        child: Scaffold(
+    return Scaffold(
       backgroundColor: const Color(0xfff2faff),
-      body: SingleChildScrollView(
-        child: Container(
-          width: double.infinity,
-          padding: const EdgeInsets.only(top: 60, left: 20, right: 20),
-          child: Column(
-            children: [
-              Text("Welcome to JAB!",
-                  style: GoogleFonts.workSans(
-                      fontWeight: FontWeight.bold, fontSize: 30)),
-              const SizedBox(height: 40),
-              LoginButtonWidget(
-                  buttonImagePath: "assets/images/google-logo.png",
-                  buttonText: "Login with Google",
-                  buttonFunction: loginWithGoogleHandler),
-              const SizedBox(height: 10),
-              LoginButtonWidget(
-                  buttonImagePath: "assets/images/apple-logo.png",
-                  buttonText: "Login with Apple",
-                  buttonFunction: loginWithAppleHandler),
-              const SizedBox(height: 40),
-              Text("or by email",
-                  style: GoogleFonts.workSans(
-                      fontWeight: FontWeight.normal,
-                      fontSize: 20,
-                      color: Colors.grey)),
-              const SizedBox(height: 40),
-              const SignStyledTextField(
-                  hintText: " Email Address", isSuffixIconActive: false),
-              const SizedBox(height: 20),
-              const SignStyledTextField(
-                  hintText: " Password", isSuffixIconActive: true),
-              const SizedBox(height: 16),
-              Row(
-                children: [
-                  const Spacer(),
-                  GestureDetector(
-                      onTap: forgotPasswordHandler,
-                      child: Text("Forgot password?",
-                          style: GoogleFonts.workSans(
-                              fontWeight: FontWeight.normal, fontSize: 16)))
-                ],
-              ),
-              const SizedBox(height: 50),
-              GestureDetector(
-                onTap: signInButtonHandler,
-                child: Container(
-                  height: 70,
-                  width: double.infinity,
-                  decoration: const BoxDecoration(
-                      color: Color(0xff0098FF),
-                      borderRadius: BorderRadius.all(Radius.circular(10))),
+      body: SafeArea(
+        child: SingleChildScrollView(
+          child: Container(
+            width: double.infinity,
+            padding: const EdgeInsets.only(top: 60, left: 20, right: 20),
+            child: Column(
+              children: [
+                Text("Welcome to JAB!",
+                    style: GoogleFonts.workSans(
+                        fontWeight: FontWeight.bold, fontSize: 30)),
+                const SizedBox(height: 40),
+                LoginButtonWidget(
+                    buttonImagePath: "assets/images/google-logo.png",
+                    buttonText: "Login with Google",
+                    buttonFunction: loginWithGoogleHandler),
+                const SizedBox(height: 10),
+                LoginButtonWidget(
+                    buttonImagePath: "assets/images/apple-logo.png",
+                    buttonText: "Login with Apple",
+                    buttonFunction: loginWithAppleHandler),
+                const SizedBox(height: 40),
+                Text("or by email",
+                    style: GoogleFonts.workSans(
+                        fontWeight: FontWeight.normal,
+                        fontSize: 20,
+                        color: Colors.grey)),
+                const SizedBox(height: 40),
+                const SignStyledTextField(
+                    hintText: " Email Address", isSuffixIconActive: false),
+                const SizedBox(height: 20),
+                const SignStyledTextField(
+                    hintText: " Password", isSuffixIconActive: true),
+                const SizedBox(height: 16),
+                Row(
+                  children: [
+                    const Spacer(),
+                    GestureDetector(
+                        onTap: forgotPasswordHandler,
+                        child: Text("Forgot password?",
+                            style: GoogleFonts.workSans(
+                                fontWeight: FontWeight.normal, fontSize: 16)))
+                  ],
+                ),
+                const SizedBox(height: 50),
+                GestureDetector(
+                  onTap: signInButtonHandler,
+                  child: Container(
+                    height: 70,
+                    width: double.infinity,
+                    decoration: const BoxDecoration(
+                        color: Color(0xff0098FF),
+                        borderRadius: BorderRadius.all(Radius.circular(10))),
+                    child: Row(
+                      mainAxisAlignment: MainAxisAlignment.center,
+                      children: [
+                        Text("Sign In",
+                            style: GoogleFonts.workSans(
+                                fontWeight: FontWeight.bold,
+                                fontSize: 25,
+                                color: Colors.white)),
+                        const SizedBox(width: 16),
+                        const Icon(Icons.arrow_forward_rounded,
+                            size: 20, color: Colors.white)
+                      ],
+                    ),
+                  ),
+                ),
+                const SizedBox(height: 20),
+                Center(
                   child: Row(
                     mainAxisAlignment: MainAxisAlignment.center,
                     children: [
-                      Text("Sign In",
-                          style: GoogleFonts.workSans(
-                              fontWeight: FontWeight.bold,
-                              fontSize: 25,
-                              color: Colors.white)),
-                      const SizedBox(width: 16),
-                      const Icon(Icons.arrow_forward_rounded,
-                          size: 20, color: Colors.white)
-                    ],
-                  ),
-                ),
-              ),
-              const SizedBox(height: 20),
-              Center(
-                child: Row(
-                  mainAxisAlignment: MainAxisAlignment.center,
-                  children: [
-                    Text("Don't have an account?",
-                        style: GoogleFonts.workSans(
-                          fontWeight: FontWeight.normal,
-                          fontSize: 16,
-                        )),
-                    GestureDetector(
-                      onTap: pushToSignUpAccountHandler,
-                      child: Text(" Create an account",
+                      Text("Don't have an account?",
                           style: GoogleFonts.workSans(
                             fontWeight: FontWeight.normal,
                             fontSize: 16,
-                            color: const Color(0xff0098FF),
                           )),
-                    )
-                  ],
-                ),
-              )
-            ],
+                      GestureDetector(
+                        onTap: pushToSignUpAccountHandler,
+                        child: Text(" Create an account",
+                            style: GoogleFonts.workSans(
+                              fontWeight: FontWeight.normal,
+                              fontSize: 16,
+                              color: const Color(0xff0098FF),
+                            )),
+                      )
+                    ],
+                  ),
+                )
+              ],
+            ),
           ),
         ),
       ),
-    ));
+    );
   }
 }
