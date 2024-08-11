a684bf370a6d64b74f173c5ad84a92a307ff8111
Amir-HShahi <amir13831107h@gmail.com>
2024-08-11 23:54:57 +0330

    ProfileScreen: notification bar color correction

--- lib/view/profile_screen.dart
+++ lib/view/profile_screen.dart
@@ -7,35 +7,36 @@ class ProfileScreen extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return SafeArea(
-        child: Scaffold(
+    return Scaffold(
       backgroundColor: const Color(0xfff2faff),
-      body: SingleChildScrollView(
-        child: Container(
-          width: double.infinity,
-          padding: const EdgeInsets.only(top: 60, left: 20, right: 20),
-          child: Column(
-            crossAxisAlignment: CrossAxisAlignment.center,
-            children: [
-              Text(
-                "My name",
-                style: GoogleFonts.poppins(
-                    fontSize: 20, fontWeight: FontWeight.bold),
-              ),
-              const SizedBox(height: 55),
-              const ShowUserDataWidget(
-                  fieldTitle: "Email",
-                  fieldIcon: Icons.email_outlined,
-                  fieldData: "xxx@gmail.com"),
-              const SizedBox(height: 20),
-              const ShowUserDataWidget(
-                  fieldTitle: "Phone Number",
-                  fieldIcon: Icons.phone_outlined,
-                  fieldData: "09172341234"),
-            ],
+      body: SafeArea(
+        child: SingleChildScrollView(
+          child: Container(
+            width: double.infinity,
+            padding: const EdgeInsets.only(top: 60, left: 20, right: 20),
+            child: Column(
+              crossAxisAlignment: CrossAxisAlignment.center,
+              children: [
+                Text(
+                  "My name",
+                  style: GoogleFonts.poppins(
+                      fontSize: 20, fontWeight: FontWeight.bold),
+                ),
+                const SizedBox(height: 55),
+                const ShowUserDataWidget(
+                    fieldTitle: "Email",
+                    fieldIcon: Icons.email_outlined,
+                    fieldData: "xxx@gmail.com"),
+                const SizedBox(height: 20),
+                const ShowUserDataWidget(
+                    fieldTitle: "Phone Number",
+                    fieldIcon: Icons.phone_outlined,
+                    fieldData: "09172341234"),
+              ],
+            ),
           ),
         ),
       ),
-    ));
+    );
   }
 }
